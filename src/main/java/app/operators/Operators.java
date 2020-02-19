a6dab7947d3503e325601d6dc69f7dde74eb6541
up201809186@fe.up.pt <3Wasd_Fra!>
2020-02-19 10:33:31 +0000

    Implemented ArithmeticOperatorDeletion UI

--- src/main/java/app/operators/Operators.java
+++ src/main/java/app/operators/Operators.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public abstract class Operators {
 
-    public static Operators[] assignedOperators = {new ArithmeticOperators(),new BitwiseOperators(),new ConditionalOperators(),new RelationalOperators(), new UnaryOperators()};
+    public static Operators[] assignedOperators = {new ArithmeticOperatorDeletion(),  new ArithmeticOperators(),new BitwiseOperators(),new ConditionalOperators(),new RelationalOperators(), new UnaryOperators()};
 
     public abstract List<List<String>> getMutators();
     public abstract List<String> getOperators();
@@ -20,18 +20,24 @@ public abstract class Operators {
         StringBuilder mutatorString = new StringBuilder();
 
         for(Operators operators : assignedOperators)
-            for (String identifier : operators.getIdentifiers()){
-                List<String> selectedMutators = (List<String>) dataStore.get(identifier);
-                for(String mutator : selectedMutators){
-                    mutatorString.append("\tnew ");
-                    mutatorString.append(operators.getMutatorType());
-                    mutatorString.append("(\"");
-                    mutatorString.append(mutator);
-                    mutatorString.append("\",\"");
-                    mutatorString.append(identifier.substring(identifier.indexOf(' ')+1));
-                    mutatorString.append("\"),\n");
+            if(operators.getMutatorType().equals("UnaryMutator") || operators.getMutatorType().equals("BinaryMutator"))
+                for (String identifier : operators.getIdentifiers()){
+                    List<String> selectedMutators = (List<String>) dataStore.get(identifier);
+                    for(String mutator : selectedMutators){
+                        mutatorString
+                                .append("\tnew ")
+                                .append(operators.getMutatorType())
+                                .append("(\"")
+                                .append(mutator)
+                                .append("\",\"")
+                                .append(identifier.substring(identifier.indexOf(' ')+1))
+                                .append("\"),\n");
+                    }
                 }
-            }
+            else if(operators instanceof ArithmeticOperatorDeletion)
+                mutatorString.append("\tnew ")
+                        .append(operators.getMutatorType())
+                        .append("(),\n");
             mutatorString.replace(mutatorString.lastIndexOf(","),mutatorString.length()," ");
         return mutatorString.toString();
     }
