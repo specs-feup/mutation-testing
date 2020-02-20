3aaff2f65c3cb7344d571065aff77ae1b0a70bdb
up201809186@fe.up.pt <3Wasd_Fra!>
2020-02-20 14:00:34 +0000

    Implemented ConstantDeletionMutator on UI

---
+++ src/main/java/app/operators/ConstantDeletionMutator.java
@@ -0,0 +1,76 @@
+package app.operators;
+
+import org.suikasoft.jOptions.Datakey.DataKey;
+import org.suikasoft.jOptions.Datakey.KeyFactory;
+import org.suikasoft.jOptions.Interfaces.DataStore;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public final class ConstantDeletionMutator extends Operators{
+
+    /***
+     * Conditional Operators
+     */
+
+    private static List<String> identifiers = new ArrayList<>();
+    private final static String DESCRIPTION = "Constant Deletion Mutator";
+    public static final String MUTATOR_TYPE = "ConstantDeletionMutator";
+
+
+    @Override
+    public List<List<String>> getMutators() {
+        return null;
+    }
+
+    @Override
+    public List<String> getOperators() {
+        return null;
+    }
+
+    @Override
+    public List<DataKey> getDataKeys()  {
+        List<DataKey> dataKeysList = new ArrayList<>();
+
+        identifiers.add(getDescription());
+        dataKeysList.add(
+                KeyFactory.string(getDescription(), "").setLabel(getDescription())
+        );
+
+        return dataKeysList;
+    }
+
+    @Override
+    public List<String> getIdentifiers() {
+        return Collections.singletonList(getDescription());
+    }
+
+    @Override
+    public String getDescription() {
+        return DESCRIPTION;
+    }
+
+    @Override
+    public String getMutatorType() {
+        return MUTATOR_TYPE;
+    }
+
+    @Override
+    public String getMutatorString(DataStore dataStore) {
+        StringBuilder mutatorString = new StringBuilder();
+
+        for (String identifier : this.getIdentifiers()){
+            String selectedMutators = (String) dataStore.get(identifier);
+
+            if(!selectedMutators.isBlank())
+                mutatorString
+                        .append("\tnew ConstantDeletionMutator(undefined, '")
+                        .append(selectedMutators)
+                        .append("'),\n");
+        }
+
+        return mutatorString.toString();
+    }
+}
