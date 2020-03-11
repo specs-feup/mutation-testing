69a313ba723b6e33dc271c8741fe90c2a0c93cdb
Francisco Azevedo <up201809186@fe.up.pt>
2020-03-11 23:22:06 +0000

    Try with multithread, one instance of Kadabra for each file

--- src/main/java/app/MutatorKernel.java
+++ src/main/java/app/MutatorKernel.java
@@ -15,54 +15,120 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Collectors;
 
 public class MutatorKernel implements AppKernel {
 
     public int execute(DataStore dataStore) {
         System.out.println("CONFIG:" + dataStore);
 
-        JSONObject jsonObject = new JSONObject();
 
-        JSONObject laraArguments = new JSONObject();
-        String projectPath = dataStore.get(Tese_UI.PROJECT_FILE).getAbsolutePath();
+        File projectPath = dataStore.get(Tese_UI.PROJECT_FILE);
         String laraPath = dataStore.get(Tese_UI.LARA_FILE).isFile() ? dataStore.get(Tese_UI.LARA_FILE).getAbsolutePath() : "src/Lara_Files/Main.lara";
         String outputPath = dataStore.get(Tese_UI.OUTPUT_FILE).getAbsolutePath() + File.separator +"Output";
 
-        List<String> arguments = new ArrayList<>(Arrays.asList(laraPath, "-p", projectPath, "-o", outputPath+"_Main"));
 
-        laraArguments.put("outputPath", outputPath);
+        List<File> filesList = getFiles(projectPath, new ArrayList<>());
 
-        String templatePath = "src/Lara_Files/template.lara";
-        String mutatorsPath =  "src/Lara_Files/Mutators.lara";
+        List<String []> listArguments = new ArrayList<>();
 
-        Replacer replacer = null;
+        if(filesList.isEmpty())
+            filesList.add(projectPath);
 
-        try {
-            replacer = new Replacer(new String(Files.readAllBytes(Paths.get(templatePath))));
-        } catch (IOException e) {
-            e.printStackTrace();
-            return -1;
+        for(File folder : filesList) {
+            JSONObject jsonObject = new JSONObject();
+
+            JSONObject laraArguments = new JSONObject();
+
+            List<String> arguments = new ArrayList<>(Arrays.asList(laraPath, "-p", folder.getAbsolutePath(), "-o", outputPath + "_Main"));
+
+            laraArguments.put("outputPath", outputPath);
+            laraArguments.put("packageName", folder.getAbsolutePath().replace(projectPath.getAbsolutePath(), "").replace(File.separatorChar, '.'));
+
+            String templatePath = "src/Lara_Files/template.lara";
+            String mutatorsPath = "src/Lara_Files/Mutators.lara";
+
+            Replacer replacer = null;
+
+            try {
+                replacer = new Replacer(new String(Files.readAllBytes(Paths.get(templatePath))));
+            } catch (IOException e) {
+                e.printStackTrace();
+                return -1;
+            }
+
+            replacer.replace("<IMPORT>", "");//Operators.getImportString());
+            replacer.replace("<MUTATORS>", Operators.generateMutatorString(dataStore));
+
+            SpecsIo.write(new File(mutatorsPath), replacer.toString());
+
+            arguments.add("-av");
+            arguments.add(laraArguments.toString());
+
+            arguments.add("-X");
+            arguments.add("-b");
+            arguments.add("2");
+            arguments.add("-s");
+            arguments.add("-Q");
+
+            System.out.println("Project path: " + projectPath);
+            System.out.println("ARGS:\n" + String.join(" ", arguments));
+
+            listArguments.add(arguments.toArray(String[]::new));
         }
 
-        replacer.replace("<IMPORT>", "");//Operators.getImportString());
-        replacer.replace("<MUTATORS>", Operators.generateMutatorString(dataStore));
-
-        SpecsIo.write(new File(mutatorsPath), replacer.toString());
-
-        arguments.add("-av");
-        arguments.add(laraArguments.toString());
-
-        arguments.add("-X");
-        arguments.add("-b");
-        arguments.add("2");
-        arguments.add("-s");
-        arguments.add("-Q");
-
-        System.out.println("Project path: " + projectPath);
-        System.out.println("ARGS:\n" + String.join(" ", arguments));
-
-        KadabraLauncher.execute(arguments.toArray(String[]::new));
+        executeParallel(listArguments.toArray(String[][]::new), 8);
 
         return 0;
+    }
+
+    public static boolean executeParallel(String [][] args, int threads) {
+
+        var customThreadPool = threads > 0 ? new ForkJoinPool(threads) : new ForkJoinPool();
+
+        System.out.println("Launching " + args.length + " instances of Kadabra in parallel, using " + threads + " threads");
+
+        try {
+            var results = customThreadPool.submit(() -> Arrays.asList(args).parallelStream()
+                    .map(MutatorKernel::executeSafe)
+                    .collect(Collectors.toList())).get();
+
+            return results.stream()
+                    .filter(result -> result == false)
+                    .findFirst()
+                    .orElse(true);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return false;
+        } catch (ExecutionException e) {
+            System.err.println("Unrecoverable exception while executing parallel instances of Clava: " + e);
+            return false;
+        }
+
+    }
+
+    private static boolean executeSafe(String[] args) {
+        try {
+            return KadabraLauncher.execute(args);
+        } catch (Exception e) {
+            System.err.println("Exception during Kadabra execution: " + e);
+            return false;
+        }
+    }
+
+    public static List<File> getFiles(File directory, List<File> list){
+        File[] fList = directory.listFiles();
+        if(fList != null)
+            for (File file : fList) {
+                if (file.isFile())
+                    list.add(file);
+                else
+                    if(file.isDirectory())
+                        list = getFiles(file, list);
+
+            }
+        return list;
     }
 }
