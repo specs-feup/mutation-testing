038405485f279c4777c296dbdff18944326d7183
RitaVeiga <93655840+RitaVeiga@users.noreply.github.com>
2022-10-05 15:55:29 +0100

    Add projects: TestProject and TestProjectOutput

---
+++ TestProject/src/main/java/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+        public static void main(String[] args) {
+            int a = 2+3;
+            int b = a +2;
+            System.out.println("B: "+ b);
+        }
+
+
+}
