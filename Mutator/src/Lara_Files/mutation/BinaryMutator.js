9126632b72b3f6b4f43151aecf1647ec7e6386c3
João Bispo <joaobispo@gmail.com>
2022-10-19 12:06:55 +0100

    [Mutator] Converts BinaryMutator from LARA to JS

--- Mutator/src/Lara_Files/mutation/BinaryMutator.js
+++ Mutator/src/Lara_Files/mutation/BinaryMutator.js
@@ -1,11 +1,11 @@
-import lara.mutation.Mutator;
-import kadabra.KadabraNodes;
-import weaver.WeaverJps;
-import weaver.Weaver;
+laraImport("lara.mutation.Mutator");
+laraImport("kadabra.KadabraNodes");
+laraImport("weaver.WeaverJps");
+laraImport("weaver.Weaver");
 
-var BinaryMutator = function($result, $original) {
-    // Parent constructor
-    Mutator.call(this);
+class BinaryMutator extends Mutator {
+  constructor($result, $original) {
+    super();
 
     this.type = "BinaryMutator";
 
@@ -15,70 +15,82 @@ var BinaryMutator = function($result, $original) {
     this.currentIndex = 0;
     this.mutationPoint = undefined;
     this.previousValue = undefined;
+  }
 
-};
+  // Inheritance
+  //BinaryMutator.prototype = Object.create(Mutator.prototype);
 
-// Inheritance
-BinaryMutator.prototype = Object.create(Mutator.prototype);
-
-BinaryMutator.prototype.getType = function(){
+  getType() {
     return this.type;
-}
+  }
 
-BinaryMutator._isCompatible = function(type1, type2) {
+  static _isCompatible(type1, type2) {
     return type1 === type2;
-}
+  }
 
-/*** IMPLEMENTATION OF INSTANCE METHODS ***/
-BinaryMutator.prototype.addJp = function($joinpoint){
-    if($joinpoint.instanceOf("binaryExpression")  &&  BinaryMutator._isCompatible($joinpoint.operator, this.$original)) {
-        this.mutationPoints.push($joinpoint);
-        return true;
+  /*** IMPLEMENTATION OF INSTANCE METHODS ***/
+  addJp($joinpoint) {
+    if (
+      $joinpoint.instanceOf("binaryExpression") &&
+      BinaryMutator._isCompatible($joinpoint.operator, this.$original)
+    ) {
+      this.mutationPoints.push($joinpoint);
+      return true;
     }
     return false;
-}
+  }
 
-
-BinaryMutator.prototype.hasMutations = function() {
+  hasMutations() {
     return this.currentIndex < this.mutationPoints.length;
-}
+  }
 
-BinaryMutator.prototype.getMutationPoint = function() {
+  getMutationPoint() {
     if (this.isMutated) {
-        return this.mutationPoint;
+      return this.mutationPoint;
     } else {
-        if (this.currentIndex < this.mutationPoints.length) {
-            return this.mutationPoints[this.currentIndex];
-        } else {
-            return undefined;
-        }
+      if (this.currentIndex < this.mutationPoints.length) {
+        return this.mutationPoints[this.currentIndex];
+      } else {
+        return undefined;
+      }
     }
-}
+  }
 
-BinaryMutator.prototype._mutatePrivate = function() {
-
+  _mutatePrivate() {
     this.mutationPoint = this.mutationPoints[this.currentIndex];
     this.currentIndex++;
 
-    if(isFunction(this.$expr)){
-        //TODO: Change
-        var temp = this.$expr(this.mutationPoint);
-        this.mutationPoint = this.mutationPoint.insertReplace(temp);
-    }else{
-        this.previousValue = this.mutationPoint.copy;
-        this.mutationPoint.operator = this.$expr;
+    if (isFunction(this.$expr)) {
+      //TODO: Change
+      var temp = this.$expr(this.mutationPoint);
+      this.mutationPoint = this.mutationPoint.insertReplace(temp);
+    } else {
+      this.previousValue = this.mutationPoint.copy;
+      this.mutationPoint.operator = this.$expr;
     }
 
     println("/*--------------------------------------*/");
-    println("Mutating operator n."+ this.currentIndex + ": "+ this.previousValue +" to "+ this.mutationPoint);
+    println(
+      "Mutating operator n." +
+        this.currentIndex +
+        ": " +
+        this.previousValue +
+        " to " +
+        this.mutationPoint
+    );
     println("/*--------------------------------------*/");
+  }
 
-}
-
-BinaryMutator.prototype._restorePrivate = function() {
+  _restorePrivate() {
     // Restore operator
-    println("Restoring: " + this.mutationPoint.operator + " to " + this.previousValue.operator);
+    println(
+      "Restoring: " +
+        this.mutationPoint.operator +
+        " to " +
+        this.previousValue.operator
+    );
     this.mutationPoint.operator = this.previousValue.operator;
     this.previousValue = undefined;
     this.mutationPoint = undefined;
+  }
 }
