abd6e8d307acf3dfa52831e60cd9a2e10f46b2d7
David Mata <52453479+davidrobert99@users.noreply.github.com>
2022-11-12 00:33:49 +0000

    Update creation of metamutant
    Erros: overriting files and not recovering original code

--- MutatorV2/javascript_src/Main.js
+++ MutatorV2/javascript_src/Main.js
@@ -12,6 +12,7 @@ const debugMessages = laraArgs.debugMessages;
 const fileName = filePath.substring(
   filePath.lastIndexOf(Io.getSeparator()) + 1
 );
+
 main();
 
 function main() {
@@ -31,156 +32,28 @@ function main() {
   if (traditionalMutation) {
     println("Going through AST for file " + fileName);
     //Goes to each node and stores the mutatation point
-    runTreeAndGetMutants();
+    runTreeAndGetMutantsTraditionaly();
 
     println("Generating Mutants for file " + fileName);
     //Goes to each stored mutation point and applies the mutation
     applyTraditionalMutation();
-
-    /*
-    println(Query.root().code);*/
   } else {
-    var counter = 0;
-    var identifiersList = new Object();
-
-    identifiersList.identifiers = [];
-
-    for (var $jp of Query.root().descendants) {
-      var $call = $jp.ancestor("call");
-
-      // Ignore nodes that are children of $call with the name <init>
-      if ($call !== undefined && $call.name === "<init>") continue;
-
-      // Ignore nodes inside variable declarations
-      //if ($jp.ancestor("localVariable") !== undefined) {
-      //  continue;
-      //}
-
-      for (mutator of Mutators) {
-        if (mutator.addJp($jp)) {
-          var fileName2 =
-            $jp.ancestor("file") === undefined
-              ? "NOFILENAME"
-              : $jp.ancestor("file").name;
-          try {
-            println(
-              "New mutation point of type " +
-                mutator.getType() +
-                " on: " +
-                $jp +
-                " file " +
-                fileName2 +
-                " line " +
-                $jp.line
-            );
-          } catch (e) {
-            try {
-              println(
-                "New mutation point of type " +
-                  mutator.getType() +
-                  " on: " +
-                  $jp.parent +
-                  " file " +
-                  fileName2 +
-                  " line " +
-                  $jp.parent.line
-              );
-            } catch (ee) {
-              println(ee);
-            }
-          }
-        }
-      }
-    }
-
-    for (mutator of Mutators) {
-      while (mutator.hasMutations()) {
-        // Mutate
-        mutator.mutate();
-        // Print
-        var identifier = new Object();
-
-        var mutated = mutator.getMutationPoint().isStatement
-          ? mutator.getMutationPoint()
-          : mutator.getMutationPoint().ancestor("statement");
-
-        println(
-          "Ancestor ->  " + mutator.getMutationPoint().ancestor("statement")
-        );
-
-        try {
-          identifier.file =
-            mutated.ancestor("file") === undefined
-              ? "NOFILENAME"
-              : mutated.ancestor("file").path;
-          identifier.line = mutated.line;
-          identifier.id =
-            fileName2 +
-            "_" +
-            mutator.getType() +
-            "_" +
-            identifier.line +
-            "_" +
-            counter;
-          println(
-            "New identifier! File -> " +
-              identifier.file +
-              " | Line -> " +
-              identifier.line +
-              " | id -> " +
-              identifier.id
-          );
-        } catch (e) {
-          println("ERROR generating ID!! " + e);
-          continue;
-        }
-        try {
-          mutated.insertBefore(
-            'if(System.getProperty("MUID") == "' +
-              identifier.id +
-              '"){\n' +
-              mutated.srcCode +
-              "\n}else{\n"
-          );
-          mutator.getMutationPoint().insertAfter("}");
-        } catch (e) {
-          try {
-            mutated = mutated.parent;
-
-            mutated.insertBefore(
-              'if(System.getProperty("MUID") == "' +
-                identifier.id +
-                '"){\n' +
-                mutated.srcCode +
-                "\n}else{\n"
-            );
-            mutated.insertAfter("}");
-          } catch (ee) {
-            println("ERROR MUTATING!!! -> " + ee);
-            continue;
-          }
-        }
-
-        identifiersList.identifiers.push(identifier);
-        counter++;
-
-        //println(mutator.getMutationPoint().parent.code);
-
-        // Restore operator
-        mutator.restore();
-      }
-    }
-    saveFile();
-    println(
-      "Finalized with " +
-        identifiersList.identifiers.length +
-        " mutants generated"
-    );
+    runTreeAndApplyMetaMutant();
   }
 }
 
-function runTreeAndGetMutants() {
+function runTreeAndGetMutantsTraditionaly() {
   for (var $jp of Query.root().descendants) {
+    var $call = $jp.ancestor("call");
+
+    // Ignore nodes that are children of $call with the name <init>
+    if ($call !== undefined && $call.name === "<init>") continue;
+
+    // Ignore nodes inside variable declarations
+    //if ($jp.ancestor("localVariable") !== undefined) {
+    //  continue;
+    //}
+
     for (mutator of Mutators) {
       if (mutator.addJp($jp)) {
         debug(mutator);
@@ -200,11 +73,103 @@ function printMutationPoints() {
 function applyTraditionalMutation() {
   for (mutator of Mutators) {
     while (mutator.hasMutations()) {
+      //Aplies the mutation
       mutator.mutate();
 
+      //Saves to a file
       saveFileNew(mutator.getName());
     }
   }
+}
+
+function runTreeAndApplyMetaMutant() {
+  for (var $jp of Query.root().descendants) {
+    var $call = $jp.ancestor("call");
+
+    // Ignore nodes that are children of $call with the name <init>
+    if ($call !== undefined && $call.name === "<init>") continue;
+
+    // Ignore nodes inside variable declarations
+    //if ($jp.ancestor("localVariable") !== undefined) {
+    //  continue;
+    //}
+
+    let mutationPoints = 0;
+    let needElseIf = false;
+    let firstTime = true;
+    for (mutator of Mutators) {
+      if (mutator.addJp($jp)) {
+        mutationPoints++;
+        debug(mutator);
+      }
+    }
+
+    if (mutationPoints >= 2) {
+      needElseIf = true;
+    }
+
+    for (mutator of Mutators) {
+      while (mutator.hasMutations()) {
+        // Mutate
+        mutator.mutate();
+
+        var mutated = mutator.getMutationPoint().isStatement
+          ? mutator.getMutationPoint()
+          : mutator.getMutationPoint().ancestor("statement");
+
+        let mutantId =
+          mutator.getName() +
+          "_" +
+          fileName.replace(".java", "") +
+          "_" +
+          Strings.uuid();
+
+        if (needElseIf) {
+          if (mutationPoints > 1) {
+            if (firstTime) {
+              mutated.insertBefore(
+                'if(System.getProperty("MUID") == "' +
+                  mutantId +
+                  '"){\n' +
+                  mutated.srcCode +
+                  ";\n}"
+              );
+
+              firstTime = false;
+            } else {
+              mutated.insertBefore(
+                'else if(System.getProperty("MUID") == "' +
+                  mutantId +
+                  '"){\n' +
+                  mutated.srcCode +
+                  ";\n}"
+              );
+            }
+            mutationPoints--;
+          } else {
+            mutated.insertBefore(
+              'else if (System.getProperty("MUID") == "' +
+                mutantId +
+                '"){\n' +
+                mutated.srcCode +
+                ";\n}else{\n\t"
+            );
+            mutated.insertAfter("}");
+          }
+        } else {
+          mutated.insertBefore(
+            'if (System.getProperty("MUID") == "' +
+              mutantId +
+              '"){\n' +
+              mutated.srcCode +
+              ";\n}else{\n\t"
+          );
+          mutated.insertAfter("}");
+        }
+      }
+    }
+  }
+  saveFileSimple();
 }
 
 function saveFileNew(mutatorName) {
@@ -229,27 +194,15 @@ function saveFileNew(mutatorName) {
   );
 }
 
-function saveFile() {
-  var $outputFolder = Io.mkdir(outputFolder + "\\src\\main\\java\\");
-  //Io.deleteFolderContents(outputFolder);
+function saveFileSimple() {
+  let relativePath = Io.getRelativePath(filePath, projectPath);
 
-  // Write modified code
-  //Weaver.writeCode($outputFolder);
+  Io.copyFolder(projectPath, outputPath, true);
 
-  // Print contents
-  // 	for(var mutatedFile of Io.getFiles(outputFolder, "*.java") ) {
-  // 		println("<File '" + mutatedFile.getName() + "'>");
-  // 		println(Io.readFile(mutatedFile));
-  // 	}
-  /*if (identifiersList.identifiers.length > 0)
-    Io.writeJson(
-      outputPath +
-        Io.getSeparator() +
-        "mutantsIdentifiers" +
-        Io.getSeparator() +
-        fileName +
-        ".json",
-      identifiersList
-    );*/
+  Io.writeFile(
+    outputPath +
+      Io.getSeparator() +
+      relativePath.replace("/", Io.getSeparator()),
+    Query.root().code
+  );
 }
-//end
--- MutatorV2/javascript_src/Mutators.js
+++ MutatorV2/javascript_src/Mutators.js
@@ -3,5 +3,12 @@ laraImport("mutation.*");
 
 
 var Mutators = [
-	new BinaryMutator("+","*") 
+	new BinaryMutator("+","*"),
+	new BinaryMutator("+","-"),
+	new BinaryMutator("+","/"),
+	new BinaryMutator("+","%"),
+	new BinaryMutator("-","+"),
+	new BinaryMutator("-","/"),
+	new BinaryMutator("-","*"),
+	new BinaryMutator("-","%") 
 ];
